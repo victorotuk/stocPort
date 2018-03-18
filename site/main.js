28ed3dffcb6f170b26ec197aced041ed694466e6
ZX_12 <veezyveek@gmail.com>
2018-03-18 09:13:10 -0400

    mar 17

--- site/main.js
+++ site/main.js
@@ -3,23 +3,44 @@ $(document).ready(function () {
   //bradley and everyone that called him the fuhrer will pay for treason
 
   alert("hey");
-  const urlToFetch = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&outputsize=full&apikey=demo';
-  function getData(){
 
-    $.ajax({
-        url: urlToFetch,
-        type: 'GET',
-        dataType: 'json',
+  $('#stockInput').val();
+  const url = "https://www.alphavantage.co/query";
+  const fn = '?function=TIME_SERIES_DAILY';
+  const os = '&outputsize=full';
+  const sym = '&symbol=';
+  const apiKey = '&apikey=RO6QHBGM0A4VO7NT';
 
-        success(response){
-          console.log(response);
-          return response;
-        },
-        error(jqXHR, status, errorThrown){
-          console.log(jqXHR);
-        }
-    });
-  }
-  getData();
+const urlToFetch = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&apikey=RO6QHBGM0A4VO7NT";
+
+const $responseField = $('#responseField');
+
+
+$('#sumbit').on('click', ()=>{
+
+
+
+  //display some elements from the response
+  $.ajax({
+
+      url: urlToFetch,
+      type: 'GET',
+      dataType: 'json',
+
+      success(response){
+        console.log(response);
+        //$responseField.append('<p>the stock price is: </p><p>' + response.Daily[0][1] + '</p>');
+        //const res = JSON.parse(response);
+      },
+
+      error(jqXHR, status, errorThrown){
+        console.log(jqXHR);
+
+      }
+  });
+
+
+});
+
 
 });
